c1312e3d9b179cd1e5382ea5003bcbae18f3d801
Shobhit Joshi <shobhit@xpertladr.com>
2020-12-13 10:46:51 +0530

    empty method

--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,11 @@ namespace DevopsDemo
             services.AddRazorPages();
         }
 
+        public void AddMethod(String myinput)
+        {
+            string name = "shobhit";
+        }
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
